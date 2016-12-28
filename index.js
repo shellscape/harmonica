a1eb06e1c9de5b9c691edf6e8e6babde7c5a2a81
shellscape <andrew@shellscape.org>
2016-12-28 16:42:08 -0500

    support node v4+ in more robust manner
    shout out to https://git.io/vMThM for the method.

--- index.js
+++ index.js
@@ -1,49 +1,20 @@
 'use strict';
 
 const cp = require('child_process');
-const semver = require('semver');
-
-// flags we can safely look for, by version, to determine if the harmony
-// flags have been applied. otherwise we're in an infinate child process loop.
-const breakpoints = {
-  get v4 () {
-    return {
-      flag: 'harmony_proxies',
-      test: 'Proxy'
-    };
-  },
-  get v5 () {
-    return this.v4;
-  },
-  get v6 () {
-    return {
-      flag: 'harmony_array_prototype_values',
-      test: 'Array.prototype.values'
-    };
-  },
-  get v7 () {
-    return this.v6;
-  }
-};
-
-const version = semver(process.version).major;
 
 module.exports = (flags) => {
 
-  let breakpoint = breakpoints['v' + version] || breakpoints.v4;
+  const breakpoint = !!process.execArgv.filter(arg => /\-\-(harmony[\w\-]*)/.test(arg)).length;
 
   flags = flags || ['harmony'];         // all the things by default
-  flags = flags.concat(breakpoint.flag);
   flags = Array.from(new Set(flags));   // force uniqueness
   flags = flags.map((f) => '--' + f);   // prepend --
 
-  // if our breakpoint / test feature is available, then don't spawn another
-  // process. we're good to go.
-  if (eval('typeof ' + breakpoint.test) !== 'undefined') {
+  if (breakpoint) {
     return;
   }
 
-  let command = process.argv[0], // eslint-disable-line one-var
+  let command = process.argv[0],
     script = process.argv.slice(1),
     params = flags.concat(script),
     app = cp.spawn(command, params, { stdio: 'inherit' });
@@ -54,5 +25,5 @@ module.exports = (flags) => {
 
   // Interrupt process flow in the parent
   process.once('uncaughtException', (e) => {});
-  throw 'harmony';
+  throw new Error('harmony');
 };
